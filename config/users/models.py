e137c414c3fba99919642ef4592036a254d92c9b
muiruritrevor <trevormuirurii@gmail.com>
2024-09-26 20:01:02 +0300

    added profile

--- config/users/models.py
+++ config/users/models.py
@@ -2,17 +2,25 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 class UserManager(BaseUserManager):
-    def create_user(self, email, password=None):
+    def create_user(self, email, username, password=None):
         if not email:
-            raise ValueError("Email is required")
-        user = self.model(email=self.normalize_email(email))
+            raise ValueError('Enter email address')
+        if not username:
+            raise ValueError('Enter username')
+
+        user = self.model(
+            email=self.normalize_email(email),
+            username=username
+        )
+
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_superuser(self, email, password):
+    def create_superuser(self, email, username, password):
         user = self.create_user(
             email=self.normalize_email(email),
+            username=username,
             password=password
         )
         user.is_admin = True
@@ -21,24 +29,18 @@ class UserManager(BaseUserManager):
         user.save(using=self._db)
         return user
     
-    
 class User(AbstractUser):
-
     email = models.EmailField(unique=True, max_length=255)
     username = models.CharField(max_length=255, unique=True)
-    
     objects = UserManager()
-
     USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = ['username', 'email', 'password']    
+    REQUIRED_FIELDS = ['username']    
 
-    def __str__(self):
-        return self.email
-    
 class Profile(models.Model):
-
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    bio = models.TextField(max_length=500, blank=True)
-
-    def __str__(self):
-        return self.user.username
+    pic = models.URLField(),
+    name = models.CharField(max_length=255),
+    bio = models.TextField(),
+    created_by = models.OneToOneField(User, on_delete=models.CASCADE),
+    created_at = models.DateTimeField(auto_now_add=True),
+    updated_at = models.DateTimeField(auto_now=True),
+    
