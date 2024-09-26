14f8346037a58febc40af804128589e49dc27bc0
muiruritrevor <trevormuirurii@gmail.com>
2024-09-26 20:00:31 +0300

    commit changes

--- config/users/admin.py
+++ config/users/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import User, Profile
 
-# Register your models here.
+class UserAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'is_staff')
+    search_fields = ('username', 'email')
+    list_filter = ('is_staff', 'is_superuser', 'is_active') 
+
+admin.site.register(User, UserAdmin)
+admin.site.register(Profile)
